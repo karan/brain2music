27992fea47f2ad9e761290839fe8118716034da5
Karan Goel <karan@goel.io>
2015-01-09 18:13:15 -0800

    added some more clj code. doesn't work as expected

--- writer/writer.js
+++ writer/writer.js
@@ -10,6 +10,7 @@ var written = 0;
 
 client.on('data',function(data){
   if (data.poorSignalLevel === 0) {
+    var data = {'beta_waves': (data.eegPower.lowBeta + data.eegPower.highBeta) / 2}
     fs.appendFile('/tmp/brain-data', JSON.stringify(data) + '\n', function (err) {
       if (err) throw err;
       written++;
