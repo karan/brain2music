49c5920c2b50dc44d2688d360aae21024cbb8f84
Karan Goel <karan@goel.io>
2015-01-09 21:26:48 -0800

    clean up

--- writer/writer.js
+++ writer/writer.js
@@ -1,5 +1,6 @@
 var neurosky = require('./node-neurosky');
 var fs = require('fs');
+var T = require("timbre");
 
 var client = neurosky.createClient({
   appName:'NodeNeuroSky',
@@ -9,15 +10,11 @@ var client = neurosky.createClient({
 var written = 0;
 
 client.on('data',function(data){
-  if (data.poorSignalLevel === 0) {
-    var data = {'beta_waves': (data.eegPower.lowBeta + data.eegPower.highBeta) / 2}
-    fs.appendFile('/tmp/brain-data', JSON.stringify(data) + '\n', function (err) {
-      if (err) throw err;
-      written++;
-      if (written % 5 === 0) {
-        console.log('===> Written', written);
-      }
-    });
+  if (data.poorSignalLevel === 200) {
+    console.log(data);
+  } else {
+    var beta_wave = (data.eegPower.lowBeta + data.eegPower.highBeta) / 2
+    T("sin", {freq: beta_wave, mul: 0.5}).play();
   }
 });
 
